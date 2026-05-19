c19b2637222acb16e2b2b3ccc97ae1f9ccdad183
Georgie1502 <jor_tejeda@hotmail.com>
2026-05-19 17:40:10 +0200

    Configure Express to serve React frontend for Render deployment

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const destinationsRouter = require('./routes/destinations')
 const bookingsRouter = require('./routes/bookings')
@@ -15,5 +16,12 @@ app.use('/api/bookings', bookingsRouter)
 app.use('/api/chat', chatRouter)
 
 app.get('/api/health', (req, res) => res.json({ status: 'ok', service: 'TimeTravel API' }))
+
+// Sert le frontend React buildé en production
+const distPath = path.join(__dirname, '../dist')
+app.use(express.static(distPath))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'))
+})
 
 module.exports = app
